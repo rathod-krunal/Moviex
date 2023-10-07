a1ea41bc109648cf69266cf01aec7940b9cb7433
rathod-krunal <devilrathod007@gmail.com>
2023-10-07 19:25:14 +0530

    fixed error

--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import Carousal from '../../../components/carousal/carousal';
+import Carousal from "../../../components/carousal/Carousal"
 import ContentWrapper from '../../../components/contentWrapper/ContentWrapper';
 import SwitchTabs from '../../../components/switchTabs/SwitchTabs';
 import useFetch from '../../../hooks/useFetch';
